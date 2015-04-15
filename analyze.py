10b7b91fbe2a536878be59ecea0add265a37fe94
edz504 <eddiedzhou@gmail.com>
2015-04-15 11:46:25 -0400

    analysis file added

--- analyze.py
+++ analyze.py
@@ -1,3 +1,8 @@
 import pickle
 
 messages = pickle.load(open('pdt_2015.p', 'rb'))
+
+num_likes = [len(m['favorited_by']) for m in messages]
+
+# most-like message
+print messages[num_likes.index(max(num_likes))]
